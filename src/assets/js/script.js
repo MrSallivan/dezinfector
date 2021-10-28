24a39de5f7687afb12aa77b77c532b11bdff1821
MrSallivan <nik.sallivan@gmail.com>
2021-10-28 18:38:47 +1000

    28

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -10,3 +10,9 @@ burger.addEventListener('click', () => {
 	}
 })
 
+// var myModal = document.getElementById('myModal')
+// var myInput = document.getElementById('myInput')
+
+// myModal.addEventListener('shown.bs.modal', function () {
+// 	myInput.focus()
+// })
