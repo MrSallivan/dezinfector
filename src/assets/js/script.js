fc9db1717493ee36bb6ce6977b36d81e264e56b0
MrSallivan <nik.sallivan@gmail.com>
2021-10-30 23:09:10 +1000

    end

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,33 +1,27 @@
 let burger = document.querySelector('.header-burger')
 let header__menu = document.querySelector('.header__menu')
+let body = document.querySelector('.body')
 burger.addEventListener('click', () => {
-	if (burger.classList.contains('active') && header__menu.classList.contains('active')) {
+	if (burger.classList.contains('active') && header__menu.classList.contains('active') && body.classList.contains('active')) {
 		burger.classList.remove('active')
 		header__menu.classList.remove('active')
+		body.classList.remove('active')
 	} else {
 		burger.classList.add('active')
 		header__menu.classList.add('active')
+		body.classList.add('active')
+
 	}
 })
 
-// var myModal = document.getElementById('myModal')
-// var myInput = document.getElementById('myInput')
-
-// myModal.addEventListener('shown.bs.modal', function () {
-// 	myInput.focus()
-// })
 
 let select = document.querySelector('.new-select')
-// console.log(select)
-// select.addEventListener('onclick', ()=>{
-// 	console.log(select)
-// })
 
 select.addEventListener("DOMSubtreeModified", function () {
 	if (select.textContent === 'Хабаровск') {
 		document.getElementById("header-tel").innerHTML = "+7 924 300-11-17"
 	}
-	if (select.textContent ===  'Комсомольск-на-Амуре') {
+	if (select.textContent === 'Комсомольск-на-Амуре') {
 		document.getElementById("header-tel").innerHTML = "+7 924 300-11-17"
 	}
 	if (select.textContent === 'Биробиджан') {
@@ -39,10 +33,7 @@ select.addEventListener("DOMSubtreeModified", function () {
 	if (select.textContent === 'Уссурийск') {
 		document.getElementById("header-tel").innerHTML = "+7 902 075-24-24"
 	}
-	if (select.textContent ===  'Артём') {
+	if (select.textContent === 'Артём') {
 		document.getElementById("header-tel").innerHTML = "+7 902 075-24-24"
 	}
 });
-// document.querySelector('.new-select').addEventListener("MutationObserver", function () {
-// 	document.getElementById("header-tel").innerHTML = "555-555"
-// });
