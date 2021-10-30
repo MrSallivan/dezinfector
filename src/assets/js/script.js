57448f89471fb3fea1c5e3b268362f762a42377c
MrSallivan <nik.sallivan@gmail.com>
2021-10-30 18:54:33 +1000

    citis_and_numbers

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,7 +1,7 @@
 let burger = document.querySelector('.header-burger')
 let header__menu = document.querySelector('.header__menu')
 burger.addEventListener('click', () => {
-	if (burger.classList.contains('active')&&header__menu.classList.contains('active')) {
+	if (burger.classList.contains('active') && header__menu.classList.contains('active')) {
 		burger.classList.remove('active')
 		header__menu.classList.remove('active')
 	} else {
@@ -16,3 +16,33 @@ burger.addEventListener('click', () => {
 // myModal.addEventListener('shown.bs.modal', function () {
 // 	myInput.focus()
 // })
+
+let select = document.querySelector('.new-select')
+// console.log(select)
+// select.addEventListener('onclick', ()=>{
+// 	console.log(select)
+// })
+
+select.addEventListener("DOMSubtreeModified", function () {
+	if (select.textContent === 'Хабаровск') {
+		document.getElementById("header-tel").innerHTML = "+7 924 300-11-17"
+	}
+	if (select.textContent ===  'Комсомольск-на-Амуре') {
+		document.getElementById("header-tel").innerHTML = "+7 924 300-11-17"
+	}
+	if (select.textContent === 'Биробиджан') {
+		document.getElementById("header-tel").innerHTML = "+7 924 300-11-17"
+	}
+	if (select.textContent === 'Владивосток') {
+		document.getElementById("header-tel").innerHTML = "+7 902 075-24-24"
+	}
+	if (select.textContent === 'Уссурийск') {
+		document.getElementById("header-tel").innerHTML = "+7 902 075-24-24"
+	}
+	if (select.textContent ===  'Артём') {
+		document.getElementById("header-tel").innerHTML = "+7 902 075-24-24"
+	}
+});
+// document.querySelector('.new-select').addEventListener("MutationObserver", function () {
+// 	document.getElementById("header-tel").innerHTML = "555-555"
+// });
